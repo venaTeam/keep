732cf8d33b0b3a9e5629d685640b2dc9cb404b6b
Abigolan <abigail1.golan@gmail.com>
2026-02-03 14:39:59 +0200

    first

---
+++ keep-ui/components/ui/ScreenWakeLock.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function WakeLock() {
+  useEffect(() => {
+    let wakeLock: WakeLockSentinel | null = null;
+
+    const requestWakeLock = async () => {
+      try {
+        if ("wakeLock" in navigator) {
+          wakeLock = await navigator.wakeLock.request("screen");
+          console.log("Wake Lock is active!");
+
+          wakeLock.addEventListener("release", () => {
+            console.log("Wake Lock handler released");
+          });
+        }
+      } catch (err: any) {
+        console.error(`${err.name}, ${err.message}`);
+      }
+    };
+
+    const handleVisibilityChange = () => {
+      if (wakeLock !== null && document.visibilityState === "visible") {
+        requestWakeLock();
+      }
+    };
+
+    requestWakeLock();
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    return () => {
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+      if (wakeLock !== null) {
+        wakeLock.release().then(() => {
+          wakeLock = null;
+        });
+      }
+    };
+  }, []);
+
+  return null;
+}
